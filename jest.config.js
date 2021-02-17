76049d3904d82303b44a8d5e62e9a1107a489a35
kobayashi-m42 <m42kobayashi@gmail.com>
2021-02-17 15:57:36 +0900

    :green_heart: #1 storyshotsによるスナップショットテストを追加

--- jest.config.js
+++ jest.config.js
@@ -15,5 +15,14 @@ module.exports = {
     'jsx',
     'json',
     'node'
-  ]
+  ],
+  // https://github.com/zeit/next.js/issues/8663#issue-490553899
+  globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    'ts-jest': {
+      'tsconfig': '<rootDir>/test/tsconfig.jest.json'
+    }
+  }
 };
