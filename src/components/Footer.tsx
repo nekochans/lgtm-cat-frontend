cf7e3958cbb91d4299e298a338bf61683d9ca8ac
keitakn <keita.koga.work@gmail.com>
2021-02-27 16:29:38 +0900

    :art: #14 フッターに利用規約とプライバシーポリシーへのリンクを追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Footer: React.FC = () => (
   <footer className="footer">
+    <div className="level-left">
+      <div className="level-item breadcrumb">
+        <ul>
+          <li>
+            <Link href="/terms">利用規約</Link>
+          </li>
+          <li>
+            <Link href="/privacy">プライバシーポリシー</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
     <div className="content has-text-centered">
       <p>Copyright (c) nekochans</p>
     </div>
