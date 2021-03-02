4b04da5d260238846a8691df3028a18a61451c56
keitakn <keita.koga.work@gmail.com>
2021-03-02 14:43:07 +0900

    :sparkles: #20 metaタグの内容を返す定数群を追加

---
+++ src/constants/metaTag.ts
@@ -0,0 +1,44 @@
+import { AppPathName, urlList } from './url';
+
+type AppName = 'LGTMeow';
+
+const appName: AppName = 'LGTMeow';
+
+const defaultTitle = appName;
+
+const defaultDescription =
+  'LGTMeowは可愛い猫のLGTM画像を共有出来るサービスです。';
+
+export type MetaTag = {
+  title: string;
+  ogpImgUrl: string;
+  ogpTargetUrl: string;
+  appName: AppName;
+  description?: string;
+};
+
+type MetaTagList = {
+  [key in AppPathName]: MetaTag;
+};
+
+export const metaTagList = (): MetaTagList => ({
+  top: {
+    title: defaultTitle,
+    ogpImgUrl: urlList.ogpImg,
+    ogpTargetUrl: urlList.top,
+    appName,
+    description: defaultDescription,
+  },
+  terms: {
+    title: `${defaultTitle} 利用規約`,
+    ogpImgUrl: urlList.ogpImg,
+    ogpTargetUrl: urlList.terms,
+    appName,
+  },
+  privacy: {
+    title: `${defaultTitle} プライバシーポリシー`,
+    ogpImgUrl: urlList.ogpImg,
+    ogpTargetUrl: urlList.privacy,
+    appName,
+  },
+});
