f0e2993f5a0a07815967fab69545e8d84422a700
kobayashi-m42 <m42kobayashi@gmail.com>
2021-03-15 11:56:51 +0900

    :recycle: #28 エラーページへの遷移を next/router を使うように変更

--- src/containers/RandomButton.tsx
+++ src/containers/RandomButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { urlList } from '../constants/url';
 import { useSetAppState } from '../contexts/AppStateContext';
 import { fetchRandomImageList } from '../infrastructure/repository/ImageRepository';
@@ -6,15 +7,14 @@ import RandomButton from '../components/RandomButton';
 
 const RandomButtonContainer: React.FC = () => {
   const setAppState = useSetAppState();
+  const router = useRouter();
 
   const handleRandom = async () => {
     try {
       const imageList = await fetchRandomImageList();
       setAppState({ imageList: imageList.images });
     } catch (e) {
-      if (window) {
-        window.location.href = urlList.error;
-      }
+      await router.push(urlList.error);
     }
   };
 
