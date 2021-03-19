65bb74c5151d77803b46a08d5c22b4445170d64e
kobayashi-m42 <m42kobayashi@gmail.com>
2021-03-20 01:08:32 +0900

    :recycle: #33 footerのメニューとコピーライトの位置が揃うようにスタイルを修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,19 +8,21 @@ type Props = {
 const Footer: React.FC<Props> = ({ termsLink, privacyLink }: Props) => (
   <footer className="footer">
     <div className="container">
-      <div className="level-left">
-        <div className="level-item breadcrumb">
-          <ul>
-            <li>{termsLink}</li>
-            <li>{privacyLink}</li>
-          </ul>
+      <nav className="level">
+        <div className="level-left">
+          <div className="level-item breadcrumb">
+            <ul>
+              <li>{termsLink}</li>
+              <li>{privacyLink}</li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="level-right">
-        <div className="content has-text-centered">
-          <p>Copyright (c) nekochans</p>
+        <div className="level-right">
+          <div className="content has-text-centered">
+            <p>Copyright (c) nekochans</p>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
   </footer>
 );
