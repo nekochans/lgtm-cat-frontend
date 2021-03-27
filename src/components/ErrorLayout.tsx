658fa3fb19f9054205a762e5b3a1a28de591c535
kobayashi-m42 <m42kobayashi@gmail.com>
2021-03-27 22:44:09 +0900

    404エラーページを作成

---
+++ src/components/ErrorLayout.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Footer from './Footer';
+import SimpleHeader from './SimpleHeader';
+import { pathList } from '../constants/url';
+
+type Props = {
+  children: ReactNode;
+  title: string;
+};
+
+const ErrorLayout: React.FC<Props> = ({ children, title }: Props) => (
+  <div className="hero is-fullheight">
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="robots" content="noindex" />
+    </Head>
+    <SimpleHeader
+      topLink={
+        <Link href={pathList.top}>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className="navbar-item">
+            <p className="is-size-4 has-text-black">LGTMeow</p>
+          </a>
+        </Link>
+      }
+    />
+    {children}
+    <Footer
+      termsLink={
+        <Link href={pathList.terms}>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a>利用規約</a>
+        </Link>
+      }
+      privacyLink={
+        <Link href={pathList.privacy}>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a>プライバシーポリシー</a>
+        </Link>
+      }
+    />
+  </div>
+);
+
+export default ErrorLayout;
