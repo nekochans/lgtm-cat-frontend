bc1357f5e076605d01e543a0161598077b2cee13
kobayashi-m42 <m42kobayashi@gmail.com>
2021-04-01 00:05:50 +0900

    モバイル端末で表示した際の余白を追加するため MarkdownContents コンポーネントのスタイルを修正

--- src/components/MarkdownContents.tsx
+++ src/components/MarkdownContents.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const MarkdownContents: React.FC<Props> = ({ markdown }: Props) => (
   <div className="container" style={{ display: 'block' }}>
-    <div className="content mb-6">
+    <div className="content mb-6 mx-3">
       <ReactMarkdown source={markdown} />
     </div>
   </div>
