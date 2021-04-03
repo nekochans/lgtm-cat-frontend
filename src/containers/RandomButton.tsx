b2aa89bd85f7b5e9d79aa4333d2889dba550a9f0
kobayashi-m42 <m42kobayashi@gmail.com>
2021-04-03 23:06:16 +0900

    ランダムボタンでAPI通信に失敗した場合はエラーコンポーネントを表示させる

--- src/containers/RandomButton.tsx
+++ src/containers/RandomButton.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import throttle from 'lodash/throttle';
-import { urlList } from '../constants/url';
 import { useSetAppState } from '../contexts/AppStateContext';
 import { fetchRandomImageList } from '../infrastructure/repository/ImageRepository';
 import RandomButton from '../components/RandomButton';
@@ -10,16 +8,15 @@ import { sendFetchRandomImages } from '../utils/gtag';
 
 const RandomButtonContainer: React.FC = () => {
   const setAppState = useSetAppState();
-  const router = useRouter();
 
   const handleRandom = async () => {
     try {
       const imageList = await fetchRandomImageList();
-      setAppState({ imageList: imageList.images });
+      setAppState({ imageList: imageList.images, isFailedFetchImages: false });
 
       sendFetchRandomImages('fetch_random_images_button');
     } catch (e) {
-      await router.push(urlList.error);
+      setAppState({ imageList: [], isFailedFetchImages: true });
     }
   };
   const handleRandomThrottled = throttle(() => handleRandom(), 500);
