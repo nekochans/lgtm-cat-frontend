7aabd71e1c9ca2d548b5dd730effba447c882120
keitakn <keita.koga.work@gmail.com>
2021-05-12 23:32:55 +0900

    :shirt: #77 Prettierのバージョンがあがった事でコードの整形結果が変わった部分をコミット

--- src/utils/imageData.ts
+++ src/utils/imageData.ts
@@ -3,268 +3,215 @@ import { Image } from '../domain/image';
 const imageData: Image[] = [
   {
     id: 0,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/00/35afef75-2d6d-4ca1-ab00-fb37f8848fca.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/00/35afef75-2d6d-4ca1-ab00-fb37f8848fca.webp',
   },
   {
     id: 1,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/00/71a7a8d4-33c2-4399-9c5b-4ea585c06580.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/00/71a7a8d4-33c2-4399-9c5b-4ea585c06580.webp',
   },
   {
     id: 2,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/00/98f86ac2-7227-44dd-bfc9-1d424b45813d.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/00/98f86ac2-7227-44dd-bfc9-1d424b45813d.webp',
   },
   {
     id: 3,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/00/bf3bbfb8-56d3-453d-811c-0f5fd9dfa4d0.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/00/bf3bbfb8-56d3-453d-811c-0f5fd9dfa4d0.webp',
   },
   {
     id: 4,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/00/44dc9b25-a0df-4726-a2bd-fccb1e0e832e.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/00/44dc9b25-a0df-4726-a2bd-fccb1e0e832e.webp',
   },
   {
     id: 5,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/00/62b7b519-9811-4e05-8c39-3c6dbab0a42d.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/00/62b7b519-9811-4e05-8c39-3c6dbab0a42d.webp',
   },
   {
     id: 6,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/01/6c7ab983-4aa1-4af4-ab37-f1327899cc26.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/01/6c7ab983-4aa1-4af4-ab37-f1327899cc26.webp',
   },
   {
     id: 7,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/01/e549cf62-c8e2-4729-af9e-b35e27bb34e3.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/01/e549cf62-c8e2-4729-af9e-b35e27bb34e3.webp',
   },
   {
     id: 8,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/01/e62cf588-057c-43a1-82a0-035d7c0e67bf.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/01/e62cf588-057c-43a1-82a0-035d7c0e67bf.webp',
   },
   {
     id: 9,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/03b4b6a8-931c-47cf-b2e5-ff8218a67b08.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/03b4b6a8-931c-47cf-b2e5-ff8218a67b08.webp',
   },
   {
     id: 10,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/1ddee797-84ad-42e6-850b-da1fe9d3226a.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/1ddee797-84ad-42e6-850b-da1fe9d3226a.webp',
   },
   {
     id: 11,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/26d12d67-3f54-4887-ab9f-fd04871574f0.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/26d12d67-3f54-4887-ab9f-fd04871574f0.webp',
   },
   {
     id: 12,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/31280089-90c6-44cd-8ab6-f2df95739eae.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/31280089-90c6-44cd-8ab6-f2df95739eae.webp',
   },
   {
     id: 13,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/34c7a37e-8504-4c12-924a-454d98458afd.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/34c7a37e-8504-4c12-924a-454d98458afd.webp',
   },
   {
     id: 14,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/385f6909-8dd9-41c3-874d-b36c1da54ab2.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/385f6909-8dd9-41c3-874d-b36c1da54ab2.webp',
   },
   {
     id: 15,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/3b1fbf6c-ed19-45b8-9cd7-12e5ba08774e.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/3b1fbf6c-ed19-45b8-9cd7-12e5ba08774e.webp',
   },
   {
     id: 16,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/406be7e8-8908-458e-b083-5c60cf58cf1e.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/406be7e8-8908-458e-b083-5c60cf58cf1e.webp',
   },
   {
     id: 17,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/4a77293c-cf7c-4478-b695-69fdf5baec45.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/4a77293c-cf7c-4478-b695-69fdf5baec45.webp',
   },
   {
     id: 18,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/4ecfd97c-677b-4321-95bb-7270b4ef03b5.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/4ecfd97c-677b-4321-95bb-7270b4ef03b5.webp',
   },
   {
     id: 19,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/4f33db82-4aa6-468e-b0ac-6148e245bbcf.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/4f33db82-4aa6-468e-b0ac-6148e245bbcf.webp',
   },
   {
     id: 20,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/5863a89c-4d4e-4f00-bf08-f1ed45cd23cb.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/5863a89c-4d4e-4f00-bf08-f1ed45cd23cb.webp',
   },
   {
     id: 21,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/655903e9-befc-4fc5-b937-f7faccd2ba25.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/655903e9-befc-4fc5-b937-f7faccd2ba25.webp',
   },
   {
     id: 22,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/7f3d3e00-71af-48f2-beac-5eab0cc09ff4.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/7f3d3e00-71af-48f2-beac-5eab0cc09ff4.webp',
   },
   {
     id: 23,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/8a3a1c22-1f1b-4989-9768-29d8fbf728cd.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/8a3a1c22-1f1b-4989-9768-29d8fbf728cd.webp',
   },
   {
     id: 24,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/8e1a13da-5a88-4fa6-9c7e-3c67d82adfbc.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/8e1a13da-5a88-4fa6-9c7e-3c67d82adfbc.webp',
   },
   {
     id: 25,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/a2a132bd-bd9e-47d9-9536-ca838294b323.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/a2a132bd-bd9e-47d9-9536-ca838294b323.webp',
   },
   {
     id: 26,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/a66dd7da-3105-4806-8c58-6fc66a0a3d04.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/a66dd7da-3105-4806-8c58-6fc66a0a3d04.webp',
   },
   {
     id: 27,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/afc54525-7611-43bb-b738-3cdf13947957.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/afc54525-7611-43bb-b738-3cdf13947957.webp',
   },
   {
     id: 28,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/ba923c4e-946a-4215-b074-44a3b848b637.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/ba923c4e-946a-4215-b074-44a3b848b637.webp',
   },
   {
     id: 29,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/bdfef72a-5b0a-4c85-b0db-471321443c1d.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/bdfef72a-5b0a-4c85-b0db-471321443c1d.webp',
   },
   {
     id: 30,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/c18a9260-4cc6-43f7-93e2-83ebbfb6186c.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/c18a9260-4cc6-43f7-93e2-83ebbfb6186c.webp',
   },
   {
     id: 31,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/ca3ae851-732e-4160-a214-6c918eaca85b.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/ca3ae851-732e-4160-a214-6c918eaca85b.webp',
   },
   {
     id: 32,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/d2b09644-a65d-4063-b4e8-39d9720743e0.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/d2b09644-a65d-4063-b4e8-39d9720743e0.webp',
   },
   {
     id: 33,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/d4a982e0-d3ca-4a93-83da-c5acd4726e88.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/d4a982e0-d3ca-4a93-83da-c5acd4726e88.webp',
   },
   {
     id: 34,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/e3fe2715-4155-45fc-b1cc-916db866f78f.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/e3fe2715-4155-45fc-b1cc-916db866f78f.webp',
   },
   {
     id: 35,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/ed62c9cf-ca00-47a4-ac56-d280a400f9f8.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/ed62c9cf-ca00-47a4-ac56-d280a400f9f8.webp',
   },
   {
     id: 36,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/22/ff92782d-fae7-4a7a-b042-adbfccf64826.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/22/ff92782d-fae7-4a7a-b042-adbfccf64826.webp',
   },
   {
     id: 37,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/137dbc7a-4aa2-4d99-9321-fc3e74b3ba94.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/137dbc7a-4aa2-4d99-9321-fc3e74b3ba94.webp',
   },
   {
     id: 38,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/19b15db9-ac6e-4e9c-96ab-8984a2b280ea.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/19b15db9-ac6e-4e9c-96ab-8984a2b280ea.webp',
   },
   {
     id: 39,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/224b8d11-33eb-48ac-9d86-a9b151ace825.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/224b8d11-33eb-48ac-9d86-a9b151ace825.webp',
   },
   {
     id: 40,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/27ad3952-8b59-47e0-997a-96cd727ba14d.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/27ad3952-8b59-47e0-997a-96cd727ba14d.webp',
   },
   {
     id: 41,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/46b1d895-d46b-4a5c-8fc1-78725efded9f.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/46b1d895-d46b-4a5c-8fc1-78725efded9f.webp',
   },
   {
     id: 42,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/46f702ca-43c7-49fa-8714-ecf1688c95e1.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/46f702ca-43c7-49fa-8714-ecf1688c95e1.webp',
   },
   {
     id: 43,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/474a4307-2d2b-471b-8bfb-1b73d2556d18.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/474a4307-2d2b-471b-8bfb-1b73d2556d18.webp',
   },
   {
     id: 44,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/518dea4f-a399-42fc-af5a-876355b9f660.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/518dea4f-a399-42fc-af5a-876355b9f660.webp',
   },
   {
     id: 45,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp',
   },
   {
     id: 46,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/5e6f48f1-fae3-497d-bf81-0df8223cc671.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/5e6f48f1-fae3-497d-bf81-0df8223cc671.webp',
   },
   {
     id: 47,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/6332ceea-e9cd-4c05-ae1a-6a61140966c3.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/6332ceea-e9cd-4c05-ae1a-6a61140966c3.webp',
   },
   {
     id: 48,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/667b6012-586f-4906-9092-8b385fb6fe9f.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/667b6012-586f-4906-9092-8b385fb6fe9f.webp',
   },
   {
     id: 49,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/ac9c977c-5794-4c51-b719-f715d9db2eaf.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/ac9c977c-5794-4c51-b719-f715d9db2eaf.webp',
   },
   {
     id: 50,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/aed16e28-93c1-4cd6-9969-23f4363afa83.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/aed16e28-93c1-4cd6-9969-23f4363afa83.webp',
   },
   {
     id: 51,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/b4c6324d-e358-4080-bad0-17d4821e456a.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/b4c6324d-e358-4080-bad0-17d4821e456a.webp',
   },
   {
     id: 52,
-    url:
-      'https://lgtm-images.lgtmeow.com/2021/03/16/23/e17e623f-6387-4910-9926-2cf8d429aa19.webp',
+    url: 'https://lgtm-images.lgtmeow.com/2021/03/16/23/e17e623f-6387-4910-9926-2cf8d429aa19.webp',
   },
 ];
 
