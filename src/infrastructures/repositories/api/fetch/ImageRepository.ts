134bb1d5ee265a205e6fbd487800d8f3b6f0de4f
keitakn <keita.koga.work@gmail.com>
2021-06-06 00:20:48 +0900

    :recycle: #82 より意味が分かりやすいようにinfrastructures/repositories配下のディレクトリ構成を変更

--- src/infrastructures/repositories/api/fetch/ImageRepository.ts
+++ src/infrastructures/repositories/api/fetch/ImageRepository.ts
@@ -1,7 +1,7 @@
-import { ImageList } from '../../../domain/types/image';
-import { FetchRandomImageList } from '../../../domain/repositories/imageRepository';
-import FetchRandomImageListError from '../../../domain/errors/FetchRandomImageListError';
-import { apiList } from '../../../constants/url';
+import { ImageList } from '../../../../domain/types/image';
+import { FetchRandomImageList } from '../../../../domain/repositories/imageRepository';
+import FetchRandomImageListError from '../../../../domain/errors/FetchRandomImageListError';
+import { apiList } from '../../../../constants/url';
 
 // eslint-disable-next-line import/prefer-default-export
 export const fetchRandomImageList: FetchRandomImageList = async () => {
