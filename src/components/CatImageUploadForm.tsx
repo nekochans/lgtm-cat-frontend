36f1f9fa1fcfe600ca1de117f49a21d2aa9c69f0
keitakn <keita.koga.work@gmail.com>
2021-07-03 23:52:43 +0900

    :sparkles: #75 とりあえず仮実装でアップロード出来るところまで実装

--- src/components/CatImageUploadForm.tsx
+++ src/components/CatImageUploadForm.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
-const CatImageUploadForm: React.FC = () => (
-  <form>
-    <div className="file has-name is-boxed">
-      <label className="file-label" htmlFor="cat-image-upload">
-        <input
-          className="file-input"
-          type="file"
-          name="uploadedCatImage"
-          id="cat-image-upload"
-        />
-        <span className="file-cta">
-          <span className="file-icon">
-            <i className="fas fa-upload" />
-          </span>
-          <span className="file-label">猫ちゃん画像をアップロード</span>
-        </span>
-      </label>
-    </div>
-  </form>
-);
+const acceptedTypes: string[] = ['image/png', 'image/jpg', 'image/jpeg'];
+
+const CatImageUploadForm: React.FC = () => {
+  const [uploadedCatImageUrl, setUploadedCatImageUrl] = useState<string>();
+
+  const isValidFileType = (fileType: string): boolean =>
+    acceptedTypes.includes(fileType);
+
+  const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files.length > 0) {
+      if (!isValidFileType(e.target.files[0].type)) {
+        return;
+      }
+
+      const url = URL.createObjectURL(e.target.files[0]);
+
+      setUploadedCatImageUrl(url);
+    }
+  };
+
+  return (
+    <>
+      <form method="post">
+        <div className="file has-name is-boxed">
+          <label className="file-label" htmlFor="cat-image-upload">
+            <input
+              className="file-input"
+              type="file"
+              name="uploadedCatImage"
+              id="cat-image-upload"
+              onChange={handleFileUpload}
+            />
+            <span className="file-cta">
+              <span className="file-icon">
+                <i className="fas fa-upload" />
+              </span>
+              <span className="file-label">猫ちゃん画像を選択</span>
+            </span>
+          </label>
+        </div>
+        <button className="button is-primary" type="submit">
+          アップロード
+        </button>
+      </form>
+      {uploadedCatImageUrl ? (
+        <img src={uploadedCatImageUrl} alt="preview" />
+      ) : (
+        ''
+      )}
+    </>
+  );
+};
 
 export default CatImageUploadForm;
