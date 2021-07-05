029cf09d29874d2ec9e750dff1e1f8a06d66c53a
keitakn <keita.koga.work@gmail.com>
2021-07-05 14:45:53 +0900

    :art: #75 ねこ画像の取得ボタンがあるとページの表示上都合が悪いのでSimpleLayoutを使うように変更

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { metaTagList } from '../constants/metaTag';
-import DefaultLayout from '../layouts/DefaultLayout';
 import CatImageUploadForm from '../components/CatImageUploadForm';
+import SimpleLayout from '../layouts/SimpleLayout';
 
 const UploadPage: React.FC = () => (
-  <DefaultLayout metaTag={metaTagList().top}>
+  <SimpleLayout metaTag={metaTagList().top}>
     <CatImageUploadForm />
-  </DefaultLayout>
+  </SimpleLayout>
 );
 
 export default UploadPage;
