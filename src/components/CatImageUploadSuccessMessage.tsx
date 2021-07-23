d828eb5688b85243e1ce614fe237ad02067424df
keitakn <keita.koga.work@gmail.com>
2021-07-23 15:51:11 +0900

    :memo: #95 Markdownをコピーする機能を実装したのでTODOコメントの内容を修正

--- src/components/CatImageUploadSuccessMessage.tsx
+++ src/components/CatImageUploadSuccessMessage.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// TODO 以下の課題でAPIの返り値からMarkdownソースを生成する形に変更
-// https://github.com/nekochans/lgtm-cat-frontend/issues/76
-// TODO blockquote をクリックしたらクリップボードにソースをコピーするように変更
 const CatImageUploadSuccessMessage: React.FC = () => (
   <article className="message is-success">
     <div className="message-header">
@@ -10,8 +7,8 @@ const CatImageUploadSuccessMessage: React.FC = () => (
     </div>
     <div className="message-body">
       アップロードされた画像を審査していますので、少々お待ち下さい。
-      審査に合格していれば以下のMarkdownソースで生成されたLGTM画像を確認出来ます。
       以下の画像をクリックするとMarkdownソースがコピーされます。
+      審査に合格していれば、コピーされたMarkdownソースで生成されたLGTM画像を確認出来ます。
     </div>
   </article>
 );
