5bd1f418dfa7c09e9e0f76e01560736a5533a246
keitakn <keita.koga.work@gmail.com>
2021-08-08 22:46:19 +0900

    :shirt: #76 Formatterの修正を適応

--- src/infrastructures/repositories/api/fetch/__test__/authTokenRepository/issueAccessToken.spec.ts
+++ src/infrastructures/repositories/api/fetch/__test__/authTokenRepository/issueAccessToken.spec.ts
@@ -9,7 +9,8 @@ describe('authTokenRepository.ts issueAccessToken TestCases', () => {
 
   it('should be able to issue an access token', async () => {
     const mockBody = {
-      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+      access_token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
       expires_in: 3600,
       token_type: 'Bearer',
     };
