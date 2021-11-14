4c194fa4c6e40912c78c77cc5808b2e434cb3909
keitakn <keita.koga.work@gmail.com>
2021-11-14 22:13:56 +0900

    :art: #62 GTMのほうで設定を行ったのでページビューイベントの送信処理を削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,20 @@
 import '../../styles/styles.scss';
 import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import { pageview, googleTagManagerId } from '../infrastructures/utils/gtm';
+import { googleTagManagerId } from '../infrastructures/utils/gtm';
 import GoogleTagManager, {
   GoogleTagManagerId,
 } from '../components/GoogleTagManager';
 
 import { AppStateProvider } from '../stores/contexts/AppStateContext';
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  return (
-    <AppStateProvider>
-      <GoogleTagManager
-        googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
-      />
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </AppStateProvider>
-  );
-};
+const App = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <AppStateProvider>
+    <GoogleTagManager
+      googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+    />
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <Component {...pageProps} />
+  </AppStateProvider>
+);
 
 export default App;
