85cefa6b8660c7d36be0528105e7bab2370e6c10
keitakn <keita.koga.work@gmail.com>
2021-11-26 23:13:28 +0900

    :art: #93 画像アップロードの確認時はモーダルを利用するように変更

--- src/components/CatImageUploadConfirmModal.tsx
+++ src/components/CatImageUploadConfirmModal.tsx
@@ -1,32 +1,52 @@
 import React from 'react';
+import Modal from 'react-modal';
 import UploadCatImagePreview from './UploadCatImagePreview';
 
 type Props = {
+  isOpen: boolean;
+  onClickCancel: () => void;
+  onClickUpload: () => Promise<void>;
   imagePreviewUrl: string;
 };
 
-const CatImageUploadConfirmModal: React.FC<Props> = ({ imagePreviewUrl }) => (
-  <div className="modal is-active">
-    <div className="modal-background" />
-    <div className="modal-card">
-      <header className="modal-card-head">
-        <p className="modal-card-title">猫ちゃん画像アップロード確認</p>
-        <button type="button" className="delete" aria-label="close" />
-      </header>
-      <section className="modal-card-body">
-        <strong>この画像をアップロードします。よろしいですか？</strong>
-        <UploadCatImagePreview imagePreviewUrl={imagePreviewUrl} />
-      </section>
-      <footer className="modal-card-foot">
-        <button type="button" className="button is-success">
-          アップロードする
-        </button>
-        <button type="button" className="button">
-          キャンセル
-        </button>
-      </footer>
+const CatImageUploadConfirmModal: React.FC<Props> = ({
+  isOpen,
+  onClickCancel,
+  onClickUpload,
+  imagePreviewUrl,
+}) => (
+  <Modal isOpen={isOpen} ariaHideApp={false}>
+    <div className="modal is-active">
+      <div className="modal-background" />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">猫ちゃん画像アップロード確認</p>
+          <button
+            type="button"
+            className="delete"
+            aria-label="close"
+            onClick={onClickCancel}
+          />
+        </header>
+        <section className="modal-card-body">
+          <strong>この画像をアップロードします。よろしいですか？</strong>
+          <UploadCatImagePreview imagePreviewUrl={imagePreviewUrl} />
+        </section>
+        <footer className="modal-card-foot">
+          <button
+            type="button"
+            className="button is-success"
+            onClick={onClickUpload}
+          >
+            アップロードする
+          </button>
+          <button type="button" className="button" onClick={onClickCancel}>
+            キャンセル
+          </button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </Modal>
 );
 
 export default CatImageUploadConfirmModal;
