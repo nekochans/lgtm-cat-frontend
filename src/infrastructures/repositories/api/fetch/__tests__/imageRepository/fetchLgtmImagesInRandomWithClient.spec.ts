17522f54b7eeffa0b5a7197f2a9314b983231aad
keitakn <keita.koga.work@gmail.com>
2022-01-30 14:46:33 +0900

    :green_heart: #133 テストをmswを使った形に変更

--- src/infrastructures/repositories/api/fetch/__tests__/imageRepository/fetchLgtmImagesInRandomWithClient.spec.ts
+++ src/infrastructures/repositories/api/fetch/__tests__/imageRepository/fetchLgtmImagesInRandomWithClient.spec.ts
@@ -1,70 +1,47 @@
-import fetchMock from 'fetch-mock-jest';
+/**
+ * @jest-environment jsdom
+ */
+import 'whatwg-fetch';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
 import { apiList } from '../../../../../../constants/url';
 import FetchLgtmImagesInRandomError from '../../../../../../domain/errors/FetchLgtmImagesInRandomError';
 import { isSuccessResult } from '../../../../../../domain/repositories/repositoryResult';
+import mockInternalServerError from '../../../../../../mocks/api/error/mockInternalServerError';
+import mockFetchLgtmImages from '../../../../../../mocks/api/external/lgtmeow/mockFetchLgtmImages';
+import fetchLgtmImagesMockBody from '../../../../../../mocks/api/fetchLgtmImagesMockBody';
 import { fetchLgtmImagesInRandomWithClient } from '../../imageRepository';
+
+const mockHandlers = [rest.get(apiList.fetchLgtmImages, mockFetchLgtmImages)];
+
+const mockServer = setupServer(...mockHandlers);
 
 // eslint-disable-next-line max-lines-per-function
 describe('imageRepository.ts fetchLgtmImagesInRandomWithClient TestCases', () => {
-  beforeEach(() => {
-    fetchMock.mockReset();
+  beforeAll(() => {
+    mockServer.listen();
+  });
+
+  afterEach(() => {
+    mockServer.resetHandlers();
+  });
+
+  afterAll(() => {
+    mockServer.close();
   });
 
   it('should be able to fetch LGTM Images', async () => {
-    const mockBody = {
-      lgtmImages: [
-        {
-          id: 5,
-          url: '/cat.jpeg',
-        },
-        {
-          id: 2,
-          url: '/cat2.jpeg',
-        },
-        {
-          id: 4,
-          url: '/cat2.jpeg',
-        },
-        {
-          id: 12,
-          url: '/cat2.jpeg',
-        },
-        {
-          id: 10,
-          url: '/cat2.jpeg',
-        },
-        {
-          id: 3,
-          url: '/cat.jpeg',
-        },
-        {
-          id: 11,
-          url: '/cat2.jpeg',
-        },
-        {
-          id: 6,
-          url: '/cat.jpeg',
-        },
-        {
-          id: 7,
-          url: '/cat2.jpeg',
-        },
-      ],
-    };
-
-    fetchMock.get(apiList.fetchLgtmImages, { status: 200, body: mockBody });
+    const expected = fetchLgtmImagesMockBody;
 
     const lgtmImagesResponse = await fetchLgtmImagesInRandomWithClient();
 
     expect(isSuccessResult(lgtmImagesResponse)).toBeTruthy();
-    expect(lgtmImagesResponse.value).toStrictEqual(mockBody);
+    expect(lgtmImagesResponse.value).toStrictEqual(expected);
   });
 
   it('should return an Error because the HTTP status is not 200', async () => {
-    const mockBody = { message: 'Internal Server Error' };
-
-    fetchMock.get(apiList.fetchLgtmImages, { status: 500, body: mockBody });
+    mockServer.use(rest.get(apiList.fetchLgtmImages, mockInternalServerError));
 
     const lgtmImagesResponse = await fetchLgtmImagesInRandomWithClient();
 
