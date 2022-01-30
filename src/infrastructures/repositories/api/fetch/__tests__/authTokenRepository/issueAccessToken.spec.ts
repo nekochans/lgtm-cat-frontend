65c01f0896c769f0dccf36ff0581b9ff5dc8e834
keitakn <keita.koga.work@gmail.com>
2022-01-30 13:30:17 +0900

    :heavy_plus_sign: #133 mswに必要なpackageを追加

--- src/infrastructures/repositories/api/fetch/__tests__/authTokenRepository/issueAccessToken.spec.ts
+++ src/infrastructures/repositories/api/fetch/__tests__/authTokenRepository/issueAccessToken.spec.ts
@@ -1,28 +1,38 @@
-import fetchMock from 'fetch-mock-jest';
+/**
+ * @jest-environment jsdom
+ */
+import 'whatwg-fetch';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
 import { cognitoTokenEndpointUrl } from '../../../../../../constants/url';
 import { isSuccessResult } from '../../../../../../domain/repositories/repositoryResult';
+import tokenEndpoint from '../../../../../../mocks/api/external/cognito/tokenEndpoint';
 import { issueAccessToken } from '../../authTokenRepository';
 
+const mockHandlers = [rest.post(cognitoTokenEndpointUrl(), tokenEndpoint)];
+
+const mockServer = setupServer(...mockHandlers);
+
 describe('authTokenRepository.ts issueAccessToken TestCases', () => {
-  beforeEach(() => {
-    fetchMock.mockReset();
+  beforeAll(() => {
+    mockServer.listen();
+  });
+
+  afterEach(() => {
+    mockServer.resetHandlers();
+  });
+
+  afterAll(() => {
+    mockServer.close();
   });
 
   it('should be able to issue an access token', async () => {
-    const mockBody = {
-      access_token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
-      expires_in: 3600,
-      token_type: 'Bearer',
-    };
-
-    fetchMock.post(cognitoTokenEndpointUrl(), { status: 200, body: mockBody });
-
     const accessTokenResult = await issueAccessToken();
 
     const expectedValue = {
-      jwtString: mockBody.access_token,
+      jwtString:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
     };
 
     expect(isSuccessResult(accessTokenResult)).toBeTruthy();
---
+++ src/mocks/api/external/cognito/tokenEndpoint.ts
@@ -0,0 +1,20 @@
+import { ResponseResolver, MockedRequest, restContext } from 'msw';
+
+import { httpStatusCode } from '../../../../constants/httpStatusCode';
+
+const tokenEndpoint: ResponseResolver<MockedRequest, typeof restContext> = (
+  _req,
+  res,
+  ctx,
+) =>
+  res(
+    ctx.status(httpStatusCode.ok),
+    ctx.json({
+      access_token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+      expires_in: 3600,
+      token_type: 'Bearer',
+    }),
+  );
+
+export default tokenEndpoint;
