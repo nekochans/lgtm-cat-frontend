f5074de7911add33911f26e456a1a196e6a9db4f
keitakn <keita.koga.work@gmail.com>
2022-04-08 23:30:02 +0900

    :art: #146 ブラウザ側で以下のエラーが発生するので extensible-custom-error を利用しない形に変更
    TypeError: Class constructor ExtensibleCustomError cannot be invoked without 'new'

--- src/domain/errors/IssueAccessTokenError.ts
+++ src/domain/errors/IssueAccessTokenError.ts
@@ -1,3 +1,10 @@
-import ExtensibleCustomError from 'extensible-custom-error';
+export default class IssueAccessTokenError extends Error {
+  constructor(error?: string) {
+    super(error);
+    this.name = new.target.name;
 
-export default class IssueAccessTokenError extends ExtensibleCustomError {}
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
