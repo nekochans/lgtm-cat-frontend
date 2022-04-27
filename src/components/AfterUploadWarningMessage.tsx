e7d321ede1c4e71309c4650dbe21e51c19bdfebf
keitakn <keita.koga.work@gmail.com>
2022-04-27 11:10:30 +0900

    :art: #100 文言をより自然な形に変更
    https://github.com/nekochans/lgtm-cat-frontend/pull/161/files#r858918745

--- src/components/AfterUploadWarningMessage.tsx
+++ src/components/AfterUploadWarningMessage.tsx
@@ -7,7 +7,7 @@ const AfterUploadWarningMessage: React.FC = () => (
     </div>
     <div className="message-body">
       <p>
-        このモーダルを閉じてしまった場合はトップページの「新着の猫ちゃんを表示」を押下して探してみてください。
+        トップページの「新着の猫ちゃんを表示」からもアップロードした画像を確認できます。
       </p>
     </div>
   </article>
