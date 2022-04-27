cc22bc93fb702c1fa8a0f6544516660821d14bd8
keitakn <keita.koga.work@gmail.com>
2022-04-27 11:14:56 +0900

    :art: #100 事前にねこ画像かどうかを判定するようにしたので合わせて文言を修正
    https://github.com/nekochans/lgtm-cat-frontend/pull/161#issuecomment-1109988063

--- src/components/CatImageUploadSuccessMessage.tsx
+++ src/components/CatImageUploadSuccessMessage.tsx
@@ -6,7 +6,7 @@ const CatImageUploadSuccessMessage: React.FC = () => (
       <p>アップロードに成功しました🐱！</p>
     </div>
     <div className="message-body">
-      <p>アップロードされた画像を審査していますので、少々お待ち下さい。</p>
+      <p>LGTM画像を作成しているので少々お待ち下さい。</p>
       <p>
         「Markdownソースをコピー」ボタンか以下の画像をクリックするとMarkdownソースがコピーされます。
       </p>
