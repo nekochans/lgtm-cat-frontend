a3035b7c92308e615159c2df9c3be348347253a5
keitakn <keita.koga.work@gmail.com>
2022-04-28 18:00:02 +0900

    :art: #162 Vercel上で動作するように設定を環境変数から取得するように変更

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ const moduleExports = {
 
 const sentryWebpackPluginOptions = {
   silent: true,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
 };
 
 module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
