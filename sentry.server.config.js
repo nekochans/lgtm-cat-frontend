698e49613957653956343dee66c35ffca9a16d57
keitakn <keita.koga.work@gmail.com>
2022-04-28 23:39:13 +0900

    :wrench: #162 設定している環境変数だけを利用するように変更

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,11 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
-const SENTRY_ENVIRONMENT = process.env.NEXT_PUBLIC_APP_ENV;
-
 Sentry.init({
-  dsn: SENTRY_DSN,
-  environment: SENTRY_ENVIRONMENT,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.NEXT_PUBLIC_APP_ENV,
   tracesSampleRate: 1.0,
 });
