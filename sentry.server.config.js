4b5d79a81aaf3aaba7b35f039297edb071268666
keitakn <keita.koga.work@gmail.com>
2022-04-28 18:11:51 +0900

    :shirt: #162 Formatterを適応

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,7 +2,8 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-const SENTRY_ENVIRONMENT = process.env.VERCEL_ENV || process.env.NEXT_PUBLIC_VERCEL_ENV || 'development';
+const SENTRY_ENVIRONMENT =
+  process.env.VERCEL_ENV || process.env.NEXT_PUBLIC_VERCEL_ENV || 'development';
 
 Sentry.init({
   dsn: SENTRY_DSN,
