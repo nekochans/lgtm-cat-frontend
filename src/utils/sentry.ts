8d39f172e9473e604f6dc7df9f13a9820908ee70
keitakn <keita.koga.work@gmail.com>
2022-10-01 00:19:31 +0900

    :shirt: #196 以下のErrorを回避する為にtruthyな値で判定する事をやめた
    Unexpected nullable string value in conditional. Please handle the nullish/empty cases explicitly

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -2,14 +2,14 @@ import * as Sentry from '@sentry/nextjs';
 
 export const mightSetRequestIdToSentry = async (response: Response) => {
   const xRequestId = await response.headers.get('x-request-id');
-  if (xRequestId) {
+  if (xRequestId != null) {
     Sentry.configureScope((scope) => {
       scope.setTag('x_request_id', xRequestId);
     });
   }
 
   const lambdaRequestId = await response.headers.get('x-lambda-request-id');
-  if (lambdaRequestId) {
+  if (lambdaRequestId != null) {
     Sentry.configureScope((scope) => {
       scope.setTag('x_lambda_request_id', lambdaRequestId);
     });
