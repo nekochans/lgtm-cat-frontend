69879e8f794eb11a74ceff157a68212efb33f1a5
keitakn <keita.koga.work@gmail.com>
2022-10-01 00:44:48 +0900

    :fire: #196 Cookieの操作は不要になったので削除

--- src/utils/cookie.ts
+++
@@ -1,8 +0,0 @@
-import { setCookie as nookiesSetCookie } from 'nookies';
-
-export const setCookie = (key: string, value: string, maxAge: number): void => {
-  nookiesSetCookie(null, key, value, {
-    maxAge,
-    path: '/',
-  });
-};
