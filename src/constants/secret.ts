d229a84b64e722f0b367b78c85c70f1d998d1182
keitakn <keita.koga.work@gmail.com>
2022-09-30 23:22:46 +0900

    :shirt: #196 ESLintの以下のErrorを修正する為、truthyな値で判定するのをやめた
    Unexpected nullable string value in conditional. Please handle the nullish/empty cases explicitly

--- src/constants/secret.ts
+++ src/constants/secret.ts
@@ -1,6 +1,8 @@
 // このファイル内の関数はサーバーサイドでしか動作しない
 export const cognitoClientId = (): string =>
-  process.env.COGNITO_CLIENT_ID ? process.env.COGNITO_CLIENT_ID : '';
+  process.env.COGNITO_CLIENT_ID != null ? process.env.COGNITO_CLIENT_ID : '';
 
 export const cognitoClientSecret = (): string =>
-  process.env.COGNITO_CLIENT_SECRET ? process.env.COGNITO_CLIENT_SECRET : '';
+  process.env.COGNITO_CLIENT_SECRET != null
+    ? process.env.COGNITO_CLIENT_SECRET
+    : '';
