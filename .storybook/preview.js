cd078c0057dbdd06e4639acd7efbddb77c9b3b76
keitakn <keita.koga.work@gmail.com>
2022-10-16 16:22:25 +0900

    :wrench: next/future/imageをStorybook上で表示させる為にパッチを適応

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -28,6 +28,16 @@ const customViewports = {
   },
 };
 
+// https://github.com/RyanClementsHax/storybook-addon-next/issues/99#issuecomment-1247073410 に記載してある暫定対応を実施
+// https://github.com/RyanClementsHax/storybook-addon-next/pull/121 がマージされたら不要になるハズ
+import Image from 'next/future/image';
+
+const OriginalImage = Image.default;
+Object.defineProperty(Image, 'default', {
+  configurable: true,
+  value: (props) => <OriginalImage {...props} unoptimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
