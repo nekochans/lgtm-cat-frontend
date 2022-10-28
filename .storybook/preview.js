0509a294e844c8ee198964cff68d2292641148b9
keitakn <keita.koga.work@gmail.com>
2022-10-28 17:19:30 +0900

    :wrench: #211 customViewportsにデバイスを追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,6 +5,13 @@ import { initialize, mswDecorator } from 'msw-storybook-addon';
 initialize();
 
 const customViewports = {
+  iPhone11ProMax: {
+    name: 'iPhone 11 Pro Max',
+    styles: {
+      width: '414px',
+      height: '896px',
+    },
+  },
   iPhone12: {
     name: 'iPhone 12 Pro',
     styles: {
@@ -12,11 +19,39 @@ const customViewports = {
       height: '844px',
     },
   },
-  GooglePixel: {
+  iPhone13ProMax: {
+    name: 'iPhone 13 Pro Max',
+    styles: {
+      width: '428px',
+      height: '926px',
+    },
+  },
+  googlePixel3: {
     name: 'Google Pixel 3',
     styles: {
       width: '411px',
       height: '823px',
+    },
+  },
+  googlePixel5: {
+    name: 'Google Pixel 5',
+    styles: {
+      width: '393px',
+      height: '851px',
+    },
+  },
+  iPadMini: {
+    name: 'iPad mini 2〜5',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  iPadMini6: {
+    name: 'iPad mini 6',
+    styles: {
+      width: '744px',
+      height: '1133px',
     },
   },
   iPadPro11: {
