1adedff8f4b11628878cb22b1d5e7a41502124f4
keitakn <keita.koga.work@gmail.com>
2022-12-13 14:29:40 +0900

    :art: @nekochans/lgtm-cat-ui がESModule形式になったので、Result型などはimportしない形に変更

--- src/features/result.ts
+++ src/features/result.ts
@@ -1,17 +1,38 @@
-import {
-  createSuccessResult as orgCreateSuccessResult,
-  createFailureResult as orgCreateFailureResult,
-  isSuccessResult as orgIsSuccessResult,
-  isFailureResult as orgIsFailureResult,
-  Result as OrgResult,
-} from '@nekochans/lgtm-cat-ui';
+export const SuccessMarker = 'SuccessMarker';
+export const FailureMarker = 'FailureMarker';
 
-export type Result<T, E> = OrgResult<T, E>;
+export type SuccessResult<T> = { value: T; _: typeof SuccessMarker };
+export type FailureResult<E> = { value: E; _: typeof FailureMarker };
+export type Result<T, E> = SuccessResult<T> | FailureResult<E>;
 
-export const createSuccessResult = orgCreateSuccessResult;
+export const createSuccessResult = <T>(value: T): SuccessResult<T> => ({
+  value,
+  // eslint-disable-next-line id-length
+  _: SuccessMarker,
+});
 
-export const createFailureResult = orgCreateFailureResult;
+export const createFailureResult = <E>(value: E): FailureResult<E> => ({
+  value,
+  // eslint-disable-next-line id-length
+  _: FailureMarker,
+});
 
-export const isSuccessResult = orgIsSuccessResult;
+export const isSuccessResult = <T>(
+  result: Result<unknown, unknown>
+): result is SuccessResult<T> => {
+  if ('_' in result) {
+    return result._ === SuccessMarker;
+  }
 
-export const isFailureResult = orgIsFailureResult;
+  return false;
+};
+
+export const isFailureResult = <T>(
+  result: Result<unknown, unknown>
+): result is FailureResult<T> => {
+  if ('_' in result) {
+    return result._ === FailureMarker;
+  }
+
+  return false;
+};
