e3a9d0262d986f0d0ad8a9f40385b10d529b160e
keitakn <keita.koga.work@gmail.com>
2023-04-02 23:42:30 +0900

    :wrench: #222 `@/` のimport方法がJestでも使えるように設定を変更

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,16 @@
-const { defaults: tsjPreset } = require('ts-jest/presets');
+const nextJest = require('next/jest');
 
-module.exports = {
-  preset: 'ts-jest',
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const customJestConfig = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  transform: {
-    ...tsjPreset.transform,
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  coveragePathIgnorePatterns: ['<rootDir>/.storybook/__mocks/'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '^@/*/(.*)$': '<rootDir>/src/$1',
   },
 };
+
+module.exports = createJestConfig(customJestConfig);
