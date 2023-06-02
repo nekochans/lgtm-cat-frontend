773f3d8dad2b2cc76af05a8309c5463214f41e54
keitakn <keita.koga.work@gmail.com>
2023-06-02 18:16:30 +0900

    :sparkles: #253 誤ったクラス名を利用出来ないように CSS Modules の型定義を追加

---
+++ src/components/MarkdownContents/MarkdownContents.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles:
+  & Readonly<{ "wrapper": string }>
+  & Readonly<{ "wrapper": string }>
+;
+export default styles;
+//# sourceMappingURL=./MarkdownContents.module.css.d.ts.map
