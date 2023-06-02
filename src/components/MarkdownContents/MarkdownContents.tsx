3714cbff137473ba825b388d798907e7f4af0f35
keitakn <keita.koga.work@gmail.com>
2023-06-02 18:06:28 +0900

    :art: #253 styled-components なしで動作するように変更

--- src/components/MarkdownContents/MarkdownContents.tsx
+++ src/components/MarkdownContents/MarkdownContents.tsx
@@ -1,30 +1,13 @@
 import type { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
-import styled from 'styled-components';
-
-const _Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  max-width: 750px;
-  font-family: Roboto, sans-serif;
-  font-size: 20px;
-  font-style: normal;
-  line-height: 25px;
-  text-align: left;
-  overflow-wrap: normal;
-  list-style-position: inside;
-  @media (max-width: 767px) {
-    max-width: 380px;
-  }
-`;
+import styles from './MarkdownContents.module.css';
 
 export type Props = {
   markdown: string;
 };
 
 export const MarkdownContents: FC<Props> = ({ markdown }) => (
-  <_Wrapper className="markdown">
+  <div className={`markdown ${styles.wrapper}`}>
     <ReactMarkdown>{markdown}</ReactMarkdown>
-  </_Wrapper>
+  </div>
 );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,54 +1,20 @@
-import React from 'react';
-import Document, {
-  type DocumentContext,
-  type DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default class CustomDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+const CustomDocument = (): JSX.Element => {
+  return (
+    <Html prefix="og: https://ogp.me/ns#">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&family=Zen+Kaku+Gothic+New&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-    try {
-      ctx.renderPage = async () =>
-        await originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
-  render(): JSX.Element {
-    return (
-      <Html prefix="og: https://ogp.me/ns#">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&family=Zen+Kaku+Gothic+New&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default CustomDocument;
