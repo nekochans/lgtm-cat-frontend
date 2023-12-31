c3df7b9f108398219f48c4859c22784ba3fba593
keitakn <keita.koga.work@gmail.com>
2024-01-01 01:52:03 +0900

    :recycle: #249 コードをminifyしたときに this.name の内容も一緒に変化してしまうので、this.prototype.name に代入するように変更

--- src/features/errors/UploadCatImageValidationError.ts
+++ src/features/errors/UploadCatImageValidationError.ts
@@ -1,11 +1,5 @@
 export class UploadCatImageValidationError extends Error {
-  constructor(error?: string) {
-    super(error);
-    this.name = new.target.name;
-
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, this.constructor);
-    }
+  static {
+    this.prototype.name = 'UploadCatImageValidationError';
   }
 }
