910c9f27a14787bd88bbbc7be717f4ab5998825d
keitakn <keita.koga.work@gmail.com>
2024-01-17 00:07:49 +0900

    :art: #273 メンテナンスモード判定関数を @vercel/edge-config を利用した実装に変更

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export const config = {
   matcher: ['/', '/upload', '/terms', '/privacy', '/maintenance'],
 };
 
-export const middleware: NextMiddleware = (req: NextRequest) => {
+export const middleware: NextMiddleware = async (req: NextRequest) => {
   const { nextUrl } = req;
 
   if (isBanCountry(req)) {
@@ -18,7 +18,8 @@ export const middleware: NextMiddleware = (req: NextRequest) => {
     return NextResponse.rewrite(nextUrl);
   }
 
-  if (isInMaintenance()) {
+  const isInMaintenanceMode = await isInMaintenance();
+  if (isInMaintenanceMode) {
     nextUrl.pathname = '/maintenance';
 
     return NextResponse.rewrite(nextUrl);
