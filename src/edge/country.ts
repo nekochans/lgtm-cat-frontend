11a78609e5df4e717e5a3ff6b094403f81ff652e
keitakn <keita.koga.work@gmail.com>
2024-01-20 18:29:30 +0900

    :art: #274 アクセス制限付きの国コードをVercel Configから取得するように変更

--- src/edge/country.ts
+++ src/edge/country.ts
@@ -1,43 +1,13 @@
+import { get } from '@vercel/edge-config';
 import { type NextRequest } from 'next/server';
 
-const bannedCountryCodeList = [
-  'BE',
-  'BG',
-  'CZ',
-  'DK',
-  'DD',
-  'DE',
-  'EE',
-  'GB',
-  'IE',
-  'GR',
-  'ES',
-  'FR',
-  'FX',
-  'GF',
-  'HR',
-  'IT',
-  'CY',
-  'LV',
-  'LT',
-  'LU',
-  'HU',
-  'MT',
-  'NL',
-  'AT',
-  'PL',
-  'PT',
-  'RO',
-  'SI',
-  'SK',
-  'FI',
-  'SE',
-];
-
-export const isBanCountry = (req: NextRequest): boolean => {
-  const country = req.geo?.country?.toUpperCase();
-  if (country != null) {
-    return bannedCountryCodeList.includes(country);
+export const isBanCountry = async (req: NextRequest): Promise<boolean> => {
+  const bannedCountryCodeList = await get<string[]>('bannedCountryCodeList');
+  if (bannedCountryCodeList) {
+    const country = req.geo?.country?.toUpperCase();
+    if (country != null) {
+      return bannedCountryCodeList.includes(country);
+    }
   }
 
   return false;
