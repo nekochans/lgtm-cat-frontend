b04d7b9c2cea4a2d75d8bc53610a2a4111cac0d6
keitakn <keita.koga.work@gmail.com>
2024-02-18 15:24:47 +0900

    :sparkles: #281 Error発生時に利用するリトライ用のボタンを追加

---
+++ src/app/_components/RetryButton.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const styles:
+  & Readonly<{ "button": string }>
+  & Readonly<{ "button": string }>
+  & Readonly<{ "text": string }>
+  & Readonly<{ "text": string }>
+;
+export default styles;
+//# sourceMappingURL=./RetryButton.module.css.d.ts.map
