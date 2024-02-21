756f0ec1cb10d36881b4371834f43ad96a78babc
keitakn <keita.koga.work@gmail.com>
2024-02-22 00:18:54 +0900

    :bug: #289 currentUrlPath の設定を忘れていたので追加

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,18 @@
 import { httpStatusCode } from '@/constants';
+import { appPathList } from '@/features';
 import { ErrorTemplate } from '@/templates';
 import type { JSX } from 'react';
 
 const NotFound = (): JSX.Element => {
   const language = 'ja';
 
-  return <ErrorTemplate type={httpStatusCode.notFound} language={language} />;
+  return (
+    <ErrorTemplate
+      type={httpStatusCode.notFound}
+      language={language}
+      currentUrlPath={appPathList.top}
+    />
+  );
 };
 
 export default NotFound;
