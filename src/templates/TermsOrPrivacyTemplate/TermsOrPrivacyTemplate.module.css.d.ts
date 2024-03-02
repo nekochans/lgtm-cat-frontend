9b946aef7187cb01970be0bdda00509097b28c71
keitakn <keita.koga.work@gmail.com>
2024-03-03 00:37:17 +0900

    :fire: #305 不要なCSSを削除

--- src/templates/TermsOrPrivacyTemplate/TermsOrPrivacyTemplate.module.css.d.ts
+++
@@ -1,7 +0,0 @@
-declare const styles:
-  & Readonly<{ "wrapper": string }>
-  & Readonly<{ "children-wrapper": string }>
-  & Readonly<{ "children-wrapper": string }>
-;
-export default styles;
-//# sourceMappingURL=./TermsOrPrivacyTemplate.module.css.d.ts.map
