27b6762fa4c3fd8b5137f1b0566b3a365ecbbc2f
keitakn <keita.koga.work@gmail.com>
2024-03-24 00:55:15 +0900

    :sparkles: #310 LGTM画像アップロード用のRoute Handlerを定義

--- src/app/api/lgtm-images/route.ts
+++ src/app/api/lgtm-images/route.ts
@@ -2,11 +2,17 @@ import {
   isFetchLgtmImagesResponseBody,
   issueClientCredentialsAccessToken,
 } from '@/api';
+import { httpStatusCode } from '@/constants';
 import {
+  acceptedTypesImageExtensions,
   FetchLgtmImagesError,
   lgtmeowApiUrl,
+  validation,
   type LgtmImage,
+  type ValidationResult,
 } from '@/features';
+import { NextResponse } from 'next/server';
+import { z } from 'zod';
 
 export const dynamic = 'force-dynamic';
 export const runtime = 'edge';
@@ -43,6 +49,109 @@ export const GET = async (): Promise<Response> => {
 
   throw new FetchLgtmImagesError(
     `src/app/api/lgtm-images/route.ts response body is invalid ${response.status} ${response.statusText}`,
+    {
+      statusCode: response.status,
+      statusText: response.statusText,
+      headers,
+      responseBody,
+    },
+  );
+};
+
+const uploadLgtmImageRequestBodySchema = z.object({
+  image: z.string().min(1),
+  imageExtension: z.enum(acceptedTypesImageExtensions),
+});
+
+type UploadLgtmImageRequestBody = z.infer<
+  typeof uploadLgtmImageRequestBodySchema
+>;
+
+const validateUploadLgtmImageRequestBody = (
+  value: unknown,
+): ValidationResult => {
+  return validation(uploadLgtmImageRequestBodySchema, value);
+};
+
+export type UploadLgtmImageResponse = {
+  imageUrl: `https://${string}`;
+};
+
+const uploadLgtmImageResponseSchema = z.object({
+  imageUrl: z.string().url(),
+});
+
+const isUploadLgtmImageResponse = (
+  value: unknown,
+): value is UploadLgtmImageResponse => {
+  return validation(uploadLgtmImageResponseSchema, value).isValidate;
+};
+
+export const POST = async (req: Request): Promise<Response> => {
+  const requestBody = (await req.json()) as UploadLgtmImageRequestBody;
+  const validationResult = validateUploadLgtmImageRequestBody(requestBody);
+  if (!validationResult.isValidate && validationResult.invalidParams) {
+    const validationErrorBody = {
+      title: 'unprocessable entity',
+      type: 'ValidationError',
+      status: httpStatusCode.unprocessableEntity,
+      invalidParams: validationResult.invalidParams,
+    } as const;
+
+    return NextResponse.json(validationErrorBody, {
+      status: httpStatusCode.unprocessableEntity,
+      statusText: 'Unprocessable Entity',
+    });
+  }
+
+  const accessToken = await issueClientCredentialsAccessToken();
+
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  };
+
+  const response = await fetch(`${lgtmeowApiUrl()}/lgtm-images`, options);
+
+  const responseBody = (await response.json()) as unknown;
+  if (response.status !== httpStatusCode.accepted) {
+    if (response.status === httpStatusCode.payloadTooLarge) {
+      const problemDetails = {
+        title: 'failed to upload lgtm image payload too large',
+        type: 'PayloadTooLarge',
+        status: httpStatusCode.payloadTooLarge,
+      } as const;
+
+      return NextResponse.json(problemDetails, {
+        status: httpStatusCode.payloadTooLarge,
+        statusText: 'Payload Too Large',
+      });
+    }
+  }
+
+  if (isUploadLgtmImageResponse(responseBody)) {
+    return NextResponse.json(
+      {
+        createdLgtmImageUrl: responseBody.imageUrl,
+      },
+      {
+        status: httpStatusCode.accepted,
+        statusText: 'Accepted',
+      },
+    );
+  }
+
+  const headers: Record<string, string> = {};
+  response.headers.forEach((value, key) => {
+    headers[key] = value;
+  });
+
+  throw new FetchLgtmImagesError(
+    `src/app/api/lgtm-images/route.ts POST response body is invalid ${response.status} ${response.statusText}`,
     {
       statusCode: response.status,
       statusText: response.statusText,
