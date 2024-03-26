9d8a6f4da0be70cebf8358089729cd9cd7c3fc09
keitakn <keita.koga.work@gmail.com>
2024-03-26 10:24:55 +0900

    :recycle: #310 ライブラリ名ではなくAPIの種類毎にディレクトリに移動

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,2 @@
-export * from './fetch';
 export * from './cognito';
 export * from './lgtmeow';
--- src/api/lgtmeow/index.ts
+++ src/api/lgtmeow/index.ts
@@ -1,1 +1,2 @@
+export * from './lgtmImage';
 export { isFetchLgtmImagesResponseBody } from './validation';
