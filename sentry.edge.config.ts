c57c31c0371edf9da9c0a170abf281a60a4c0eba
keitakn <keita.koga.work@gmail.com>
2024-06-30 00:23:26 +0900

    :recycle: #320 Sentryの設定ファイルの書き方を最新の書き方に合わせて更新

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,7 +3,8 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NEXT_PUBLIC_APP_ENV,
-  tracesSampleRate: 0.3,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
+  tracesSampleRate: 0.1,
+  debug: false,
 });
--- sentry.server.config.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  environment: process.env.NEXT_PUBLIC_APP_ENV,
-  tracesSampleRate: 0.3,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
