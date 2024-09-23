e34c0c7c2ff7a50308db8f9c3457601dd5357987
keitakn <keita.koga.work@gmail.com>
2024-09-23 23:46:39 +0900

    :art: #324 パディングの調整を実施

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -35,85 +35,87 @@ export const Header = ({ language, currentUrlPath }: Props): JSX.Element => {
     <ReactAriaHeader className="w-full border-b border-orange-300 bg-orange-500">
       <div className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between px-5">
         <div className="flex w-full items-center justify-between pl-7">
-          <HeaderLogo language={language} />
-          <nav className="flex items-center gap-6">
-            <Link
-              href={appPathList.upload}
-              className="text-base font-medium text-orange-50 hover:text-orange-100"
-            >
-              <Text slot="label">アップロード</Text>
-            </Link>
-            <Link
-              href="/how-to-use"
-              className="text-base font-medium text-orange-50 hover:text-orange-100"
-            >
-              <Text slot="label">使い方</Text>
-            </Link>
-            <Link
-              href={terms.link}
-              className="text-base font-medium text-orange-50 hover:text-orange-100"
-            >
-              <Text slot="label">{terms.text}</Text>
-            </Link>
-            <MenuTrigger>
-              <Button className="flex items-center text-base font-medium text-orange-50 hover:text-orange-100">
-                ポリシー
-                <DownIcon />
-              </Button>
-              <Popover className="w-48 rounded-md bg-orange-500 shadow-lg ring-1 ring-black ring-opacity-5">
-                <Menu className="py-1">
-                  <MenuItem className="block w-full px-4 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
-                    <Link href={privacy.link}>
-                      <Text slot="label">{privacy.text}</Text>
-                    </Link>
-                  </MenuItem>
-                  <MenuItem className="block w-full px-4 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
-                    <Link href={externalTransmissionPolicy.link}>
-                      <Text slot="label">
-                        {externalTransmissionPolicy.text}
-                      </Text>
-                    </Link>
-                  </MenuItem>
-                </Menu>
-              </Popover>
-            </MenuTrigger>
-            <MenuTrigger>
-              <Button className="flex items-center text-base font-medium text-orange-50 hover:text-orange-100">
-                <GlobeIcon />
-                language
-                <DownIcon />
-              </Button>
-              <Popover className="w-48 rounded-md bg-orange-500 shadow-lg ring-1 ring-black ring-opacity-5">
-                <Menu className="py-1">
-                  <MenuItem
-                    className={`flex w-full items-center justify-between px-4 py-2 text-left text-base font-medium ${
-                      language === 'ja'
-                        ? 'bg-orange-600 text-orange-50'
-                        : 'text-orange-50 hover:bg-orange-600'
-                    }`}
-                  >
-                    <Link href={currentUrlPath}>
-                      <Text slot="label">日本語</Text>
-                    </Link>
-                    {language === 'ja' && <RightIcon />}
-                  </MenuItem>
-                  <MenuItem
-                    className={`flex w-full items-center justify-between px-4 py-2 text-left text-base font-medium ${
-                      language === 'en'
-                        ? 'bg-orange-600 text-orange-50'
-                        : 'text-orange-50 hover:bg-orange-600'
-                    }`}
-                  >
-                    <Link href={currentUrlPath}>
-                      <Text slot="label">English</Text>
-                    </Link>
-                    {language === 'en' && <RightIcon />}
-                  </MenuItem>
-                </Menu>
-              </Popover>
-            </MenuTrigger>
-            <LoginButton language={language} />
-          </nav>
+          <div className="flex items-center gap-24">
+            <HeaderLogo language={language} />
+            <nav className="flex items-center gap-6">
+              <Link
+                href={appPathList.upload}
+                className="text-base font-medium text-orange-50 hover:text-orange-100"
+              >
+                <Text slot="label">アップロード</Text>
+              </Link>
+              <Link
+                href="/how-to-use"
+                className="text-base font-medium text-orange-50 hover:text-orange-100"
+              >
+                <Text slot="label">使い方</Text>
+              </Link>
+              <Link
+                href={terms.link}
+                className="text-base font-medium text-orange-50 hover:text-orange-100"
+              >
+                <Text slot="label">{terms.text}</Text>
+              </Link>
+              <MenuTrigger>
+                <Button className="flex items-center text-base font-medium text-orange-50 hover:text-orange-100">
+                  ポリシー
+                  <DownIcon />
+                </Button>
+                <Popover className="w-48 rounded-md bg-orange-500 shadow-lg ring-1 ring-black ring-opacity-5">
+                  <Menu className="py-1">
+                    <MenuItem className="block w-full px-4 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
+                      <Link href={privacy.link}>
+                        <Text slot="label">{privacy.text}</Text>
+                      </Link>
+                    </MenuItem>
+                    <MenuItem className="block w-full px-4 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
+                      <Link href={externalTransmissionPolicy.link}>
+                        <Text slot="label">
+                          {externalTransmissionPolicy.text}
+                        </Text>
+                      </Link>
+                    </MenuItem>
+                  </Menu>
+                </Popover>
+              </MenuTrigger>
+              <MenuTrigger>
+                <Button className="flex items-center text-base font-medium text-orange-50 hover:text-orange-100">
+                  <GlobeIcon />
+                  language
+                  <DownIcon />
+                </Button>
+                <Popover className="w-48 rounded-md bg-orange-500 shadow-lg ring-1 ring-black ring-opacity-5">
+                  <Menu className="py-1">
+                    <MenuItem
+                      className={`flex w-full items-center justify-between px-4 py-2 text-left text-base font-medium ${
+                        language === 'ja'
+                          ? 'bg-orange-600 text-orange-50'
+                          : 'text-orange-50 hover:bg-orange-600'
+                      }`}
+                    >
+                      <Link href={currentUrlPath}>
+                        <Text slot="label">日本語</Text>
+                      </Link>
+                      {language === 'ja' && <RightIcon />}
+                    </MenuItem>
+                    <MenuItem
+                      className={`flex w-full items-center justify-between px-4 py-2 text-left text-base font-medium ${
+                        language === 'en'
+                          ? 'bg-orange-600 text-orange-50'
+                          : 'text-orange-50 hover:bg-orange-600'
+                      }`}
+                    >
+                      <Link href={currentUrlPath}>
+                        <Text slot="label">English</Text>
+                      </Link>
+                      {language === 'en' && <RightIcon />}
+                    </MenuItem>
+                  </Menu>
+                </Popover>
+              </MenuTrigger>
+            </nav>
+          </div>
+          <LoginButton language={language} />
         </div>
       </div>
     </ReactAriaHeader>
