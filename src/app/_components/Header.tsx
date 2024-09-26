5fe3555c48ebc708f55900df98db35eaa1c99212
keitakn <keita.koga.work@gmail.com>
2024-09-27 00:18:56 +0900

    :art: #324 ポリシー部分のデザインを調整しました

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -40,36 +40,39 @@ export const Header = ({ language, currentUrlPath }: Props): JSX.Element => {
             <nav className="flex items-center gap-6">
               <Link
                 href={appPathList.upload}
-                className="text-base font-medium text-orange-50 hover:text-orange-100"
+                className="flex items-center justify-center bg-orange-500 p-5 text-base font-medium text-orange-50 hover:text-orange-100"
               >
                 <Text slot="label">アップロード</Text>
               </Link>
               <Link
                 href="/how-to-use"
-                className="text-base font-medium text-orange-50 hover:text-orange-100"
+                className="flex items-center justify-center bg-orange-500 p-5 text-base font-medium text-orange-50 hover:text-orange-100"
               >
                 <Text slot="label">使い方</Text>
               </Link>
               <Link
                 href={terms.link}
-                className="text-base font-medium text-orange-50 hover:text-orange-100"
+                className="flex items-center justify-center bg-orange-500 p-5 text-base font-medium text-orange-50 hover:text-orange-100"
               >
                 <Text slot="label">{terms.text}</Text>
               </Link>
               <MenuTrigger>
-                <Button className="flex items-center justify-center gap-2 bg-orange-500 p-5 text-base font-medium text-orange-50 hover:text-orange-100">
+                <Button className="flex items-center justify-center gap-2 bg-orange-500 px-5 py-2 text-base font-medium text-orange-50 hover:text-orange-100">
                   ポリシー
                   <DownIcon />
                 </Button>
-                <Popover className="w-48 rounded-md bg-orange-500 shadow-lg ring-1 ring-black ring-opacity-5">
-                  <Menu className="py-1">
-                    <MenuItem className="block w-full px-4 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
-                      <Link href={privacy.link}>
+                <Popover className="bg-orange-500 shadow-lg ring-1 ring-black ring-opacity-5">
+                  <Menu className="w-[180px]">
+                    <MenuItem className="flex w-full items-center px-6 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
+                      <Link href={privacy.link} className="w-full">
                         <Text slot="label">{privacy.text}</Text>
                       </Link>
                     </MenuItem>
-                    <MenuItem className="block w-full px-4 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
-                      <Link href={externalTransmissionPolicy.link}>
+                    <MenuItem className="flex w-full items-center px-6 py-2 text-left text-base font-medium text-orange-50 hover:bg-orange-600">
+                      <Link
+                        href={externalTransmissionPolicy.link}
+                        className="w-full"
+                      >
                         <Text slot="label">
                           {externalTransmissionPolicy.text}
                         </Text>
@@ -83,24 +86,27 @@ export const Header = ({ language, currentUrlPath }: Props): JSX.Element => {
           <div className="flex items-center gap-4 px-7">
             <MenuTrigger>
               <Button className="flex items-center justify-center gap-2 bg-orange-500 px-5 py-2 text-base font-medium text-orange-50 hover:text-orange-100">
-                <span className="flex items-center justify-center w-5 h-5">
+                <span className="flex size-5 items-center justify-center">
                   <GlobeIcon />
                 </span>
                 language
-                <span className="flex items-center justify-center w-4 h-4">
+                <span className="flex size-4 items-center justify-center">
                   <DownIcon />
                 </span>
               </Button>
               <Popover className="bg-orange-500 shadow-lg ring-1 ring-black ring-opacity-5">
                 <Menu className="w-[167px]">
                   <MenuItem
-                    className={`flex w-full items-center py-2 px-5 text-left text-base font-medium ${
+                    className={`flex w-full items-center px-5 py-2 text-left text-base font-medium ${
                       language === 'ja'
                         ? 'bg-orange-400 text-orange-50'
                         : 'bg-orange-500 text-orange-50 hover:bg-orange-600'
                     }`}
                   >
-                    <Link href={currentUrlPath} className="flex items-center w-full">
+                    <Link
+                      href={currentUrlPath}
+                      className="flex w-full items-center"
+                    >
                       <span className="flex items-center gap-2">
                         {language === 'ja' && <RightIcon />}
                         <Text slot="label">日本語</Text>
@@ -108,13 +114,16 @@ export const Header = ({ language, currentUrlPath }: Props): JSX.Element => {
                     </Link>
                   </MenuItem>
                   <MenuItem
-                    className={`flex w-full items-center py-2 px-5 text-left text-base font-medium ${
+                    className={`flex w-full items-center px-5 py-2 text-left text-base font-medium ${
                       language === 'en'
                         ? 'bg-orange-400 text-orange-50'
                         : 'bg-orange-500 text-orange-50 hover:bg-orange-600'
                     }`}
                   >
-                    <Link href={currentUrlPath} className="flex items-center w-full">
+                    <Link
+                      href={currentUrlPath}
+                      className="flex w-full items-center"
+                    >
                       <span className="flex items-center gap-2">
                         {language === 'en' && <RightIcon />}
                         <Text slot="label">English</Text>
