1f69d993e55793ed4bc03f805ce921e608a9628f
keitakn <keita.koga.work@gmail.com>
2024-10-11 00:30:11 +0900

    :shirt: #347 以下のESLintエラーの対応
    src/utils/assertNever.ts
      1:1   error  Definition for rule '@typescript-eslint/no-unused-vars' was not found    @typescript-eslint/no-unused-vars
      2:29  error  'value' is defined but never used. Allowed unused args must match /^_/u  unused-imports/no-unused-vars

--- src/utils/assertNever.ts
+++ src/utils/assertNever.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line unused-imports/no-unused-vars
 export function assertNever(value: never): never {
   throw new Error('Unexpected value. Should have been never.');
 }
