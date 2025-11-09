e339a4d6bb087eaf36c51a57d8eb992bd9a918e0
keitakn <keita.koga.work@gmail.com>
2025-11-09 13:24:06 +0900

    :bug: #358 Next.js 16の書き方に対応

--- src/edge/url.ts
+++ src/edge/url.ts
@@ -4,8 +4,9 @@ import { appBaseUrl, isUrl, type Url } from "@/features/url";
 
 export const appBaseUrlHeaderName = "lgtmeow-app-base-url";
 
-export function extractAppBaseUrlFromHeader(): Url {
-  const url = headers().get(appBaseUrlHeaderName);
+export async function extractAppBaseUrlFromHeader(): Promise<Url> {
+  const incomingHeaders = await headers();
+  const url = incomingHeaders.get(appBaseUrlHeaderName);
   if (isUrl(url)) {
     return url;
   }
