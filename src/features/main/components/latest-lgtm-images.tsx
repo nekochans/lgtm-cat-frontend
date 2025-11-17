6678d73bf086cc85d304f6b4adc65cb3840ec24d
keitakn <keita.koga.work@gmail.com>
2025-11-17 21:20:10 +0900

    :bug: #327 不要なSuspenseが残っていたので削除

--- src/features/main/components/latest-lgtm-images.tsx
+++ src/features/main/components/latest-lgtm-images.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import { LgtmImages } from "@/features/main/components/lgtm-images";
 import { fetchLgtmImagesInRecentlyCreated } from "@/features/main/functions/fetch-lgtm-images";
 import { issueClientCredentialsAccessToken } from "@/lib/cognito/oidc";
@@ -6,15 +5,11 @@ import { issueClientCredentialsAccessToken } from "@/lib/cognito/oidc";
 const fetchLgtmImages = async () => {
   const accessToken = await issueClientCredentialsAccessToken();
 
-  return await fetchLgtmImagesInRecentlyCreated(accessToken);
+  return fetchLgtmImagesInRecentlyCreated(accessToken);
 };
 
 export const LatestLgtmImages = async () => {
   const lgtmImages = await fetchLgtmImages();
 
-  return (
-    <Suspense>
-      <LgtmImages images={lgtmImages} />
-    </Suspense>
-  );
+  return <LgtmImages images={lgtmImages} />;
 };
--- src/features/main/components/random-lgtm-images.tsx
+++ src/features/main/components/random-lgtm-images.tsx
@@ -1,5 +1,4 @@
 // 絶対厳守：編集前に必ずAI実装ルールを読む
-import { Suspense } from "react";
 import { LgtmImages } from "@/features/main/components/lgtm-images";
 import { fetchLgtmImagesInRandom } from "@/features/main/functions/fetch-lgtm-images";
 import { issueClientCredentialsAccessToken } from "@/lib/cognito/oidc";
@@ -7,15 +6,11 @@ import { issueClientCredentialsAccessToken } from "@/lib/cognito/oidc";
 const fetchLgtmImages = async () => {
   const accessToken = await issueClientCredentialsAccessToken();
 
-  return await fetchLgtmImagesInRandom(accessToken);
+  return fetchLgtmImagesInRandom(accessToken);
 };
 
 export const RandomLgtmImages = async () => {
   const lgtmImages = await fetchLgtmImages();
 
-  return (
-    <Suspense>
-      <LgtmImages images={lgtmImages} />
-    </Suspense>
-  );
+  return <LgtmImages images={lgtmImages} />;
 };
